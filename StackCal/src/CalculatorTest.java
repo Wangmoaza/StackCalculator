815e1878c1c95a66f37e4e5f1fda36001a0c734a
Wangmoaza <haeun0512@hanmail.net>
2016-04-21 18:30:15 +0900

    postfix conversion complete

--- StackCal/src/CalculatorTest.java
+++ StackCal/src/CalculatorTest.java
@@ -1,4 +1,13 @@
 import java.io.*;
+import java.util.Stack;
+import java.lang.Character;
+
+/**
+ * @author 하은
+ * need to do:
+ * 1. illegal expression 처리
+ * 3. 
+ */
 
 public class CalculatorTest
 {
@@ -28,4 +37,183 @@ public class CalculatorTest
 		// TODO : 아래 문장을 삭제하고 구현해라.
 		System.out.println("<< command 함수에서 " + input + " 명령을 처리할 예정입니다 >>");
 	}
+	
+	private static void calPostfix(String in)
+	{
+		String[] postfixArr = in.split("\\s+");
+		
+		
+	}
+	
+	class InToPost 
+	{
+		// modification from http://www.tutorialspoint.com/javaexamples/data_intopost.htm
+		private Stack<Character> theStack;
+		private String input;
+		private String output = "";
+		   
+		public InToPost(String in) 
+		{
+			input = in.trim();
+			theStack = new Stack<Character>();
+		}
+		   
+		public String doTrans() 
+		{
+			boolean binaryMinusFlag = false;
+			boolean newNumberFlag = false;
+
+			for (int j = 0; j < input.length(); j++) 
+			{
+				char ch = input.charAt(j);
+
+				if (ch == '-')
+				{
+					// discriminate unary and binary
+					// - (binary) can only appear right after ')' or digit
+		            if (binaryMinusFlag)
+		               gotOper(ch);
+
+		            else
+		               gotOper('~');
+		            
+		            binaryMinusFlag = false;
+		            newNumberFlag = true;
+		         }
+
+				else if (ch == '*' || ch == '/' || ch == '%' || ch == '+' || ch == '^' )
+				{
+					gotOper(ch);
+		            binaryMinusFlag = false;
+		            newNumberFlag = true;
+				}
+
+				else if (ch == '(')
+				{
+		            theStack.push(ch);
+		            binaryMinusFlag = false;
+		            newNumberFlag = true;
+				}
+
+				else if (ch == ')')
+				{
+		            gotParen(ch);
+		            binaryMinusFlag = true;
+		            newNumberFlag = true;
+				}
+
+				else if ('0' <= ch && ch <='9')
+				{
+		            if (newNumberFlag && output != "")
+		               output = output + " " + ch;
+		            else
+		               output = output + ch;
+		            
+		            binaryMinusFlag = true;
+		            newNumberFlag = false;
+		         }
+
+				else if (ch == '\t' || ch == ' ')
+				{
+		            newNumberFlag = true;
+				}
+
+				else
+				{
+					System.out.println("ERROR");
+					break;
+				}
+			}
+		
+			while (!theStack.isEmpty()) 
+			{
+		         output = output + " " + theStack.pop();
+			}
+		      
+			return output; 
+		}
+
+		private int precedence(char ch)
+		{
+			// precedence: lowest (1) - highest (4)
+			// 1: +, - (binary)
+			// 2: *, /, %
+			// 3: ~ (unary)
+			// 4: ^
+			switch (ch)
+			{
+		         case '+':
+		         case '-':
+		            return 1;
+		         case '*':
+		         case '/':
+		         case '%':
+		            return 2;
+		         case '~':
+		            return 3;
+		         case '^':
+		            return 4;
+		         default:
+		            System.out.println("not an operator");
+		            return 0;
+			}
+		}
+		   
+		private void gotOper(char opThis) 
+		{
+			int thisPrec = precedence(opThis);
+
+			while (!theStack.isEmpty()) 
+			{
+				char opTop = theStack.pop();
+		         
+		         if (opTop == '(') 
+		         {
+		            theStack.push(opTop);
+		            break;
+		         }
+		         
+		         else 
+		         {
+		            int topPrec = precedence(opTop);
+		            
+		            if (topPrec < thisPrec) 
+		            { 
+		               theStack.push(opTop);
+		               break;
+		            }
+
+		            else if (topPrec == thisPrec && (opThis == '^' || opThis == '~'))
+		            {
+		               theStack.push(opTop);
+		               break;
+		            }
+
+		            else
+		               output = output + " " + opTop;
+		         }
+			}
+			theStack.push(opThis);
+		}
+		   
+		private void gotParen(char ch)
+		{ 
+			while (!theStack.isEmpty()) 
+			{
+				char chx = theStack.pop();
+		         
+		         if (chx == '(') 
+		            break; 
+		         else
+		            output = output + " " + chx; 
+			}
+		}
+		
+		@Override
+		public String toString()
+		{
+			return output;
+		}
+	}
 }
+
